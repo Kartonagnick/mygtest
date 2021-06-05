35e565d4635d79a99be4908c66dffd7f21d11fc2
Kartonagnick <CastleOfDreams@yandex.ru>
2021-06-05 16:25:00 +0300

    add: modern.hpp (001) PRE

---
+++ include/mygtest/modern.hpp
@@ -0,0 +1,10 @@
+// [2021y-06m-05d][12:10:00] Idrisov Denis R. 001 PRE
+#pragma once
+
+#ifndef dMYGTEST_MODERN_USED_
+#define dMYGTEST_MODERN_USED_ 001 PRE
+
+#include <mygtest/test-list.hpp>
+#include <mygtest/private/extension.hpp>
+
+#endif // !dMYGTEST_MODERN_USED_
