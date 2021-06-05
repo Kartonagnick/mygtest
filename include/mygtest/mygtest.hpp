de7fd4278112a5d168e4ffb4f5b094cdbf12bbfc
Kartonagnick <CastleOfDreams@yandex.ru>
2021-06-05 15:42:30 +0300

    add: mygtest.hpp (001) PRE

---
+++ include/mygtest/mygtest.hpp
@@ -0,0 +1,48 @@
+// [2021y-06m-05d][12:10:00] Idrisov Denis R. 001 PRE
+//==============================================================================
+//==============================================================================
+
+#pragma once
+#ifndef dMYGTEST_USED_ 
+#define dMYGTEST_USED_ 001 PRE
+
+#if defined(__GNUC__)
+    #pragma GCC system_header
+#endif
+
+#include <mygtest/private/features.hpp>
+
+//==============================================================================
+//==============================================================================
+
+#ifdef _MSC_VER
+    #if defined(dGMOCK_LIBRARY_USED_)
+        #pragma message("InitGoogleMock...")
+
+    #elif defined(dGTEST_LIBRARY_USED_)
+        #pragma message("InitGoogleTest...")
+
+    #else
+        #pragma message("[WARNING] InitGoogleTest by default...")
+    #endif
+#endif
+
+//==============================================================================
+//==============================================================================
+
+#if defined(dGMOCK_LIBRARY_USED_)
+    #include <gtest/gtest.h>
+    #include <gmock/gmock.h>
+
+#elif defined(dGTEST_LIBRARY_USED_)
+    #include <gtest/gtest.h>
+
+#else
+    #define dGMOCK_LIBRARY_USED_ 1
+    #include <gmock/gmock.h>
+
+#endif
+
+//==============================================================================
+//==============================================================================
+#endif // !dMYGTEST_USED_
