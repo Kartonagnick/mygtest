f814ec9cf6b1efd228b9854cf9153dfa15ebc13c
Kartonagnick <CastleOfDreams@yandex.ru>
2021-06-08 12:41:15 +0300

    upd: private/features.hpp (001)

--- include/mygtest/private/features.hpp
+++ include/mygtest/private/features.hpp
@@ -1,7 +1,8 @@
+// [2021y-06m-08d][15:00:00] Idrisov Denis R. 001
 // [2021y-06m-05d][12:10:00] Idrisov Denis R. 001 PRE
 #pragma once
 #ifndef dMYGTEST_FEATURES_USED_
-#define dMYGTEST_FEATURES_USED_ 001 PRE
+#define dMYGTEST_FEATURES_USED_ 1
 
 //==============================================================================
 //=== dHAS_RVALUE_REFERENCES/dHAS_ATOMIC =======================================
