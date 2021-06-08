9927e4e39ff4d28035e1e27bff0616dad5f10a4e
Kartonagnick <CastleOfDreams@yandex.ru>
2021-06-08 12:13:45 +0300

    upd: modern.hpp (001)

--- include/mygtest/modern.hpp
+++ include/mygtest/modern.hpp
@@ -1,8 +1,9 @@
+// [2021y-06m-08d][15:00:00] Idrisov Denis R. 001
 // [2021y-06m-05d][12:10:00] Idrisov Denis R. 001 PRE
 #pragma once
 
 #ifndef dMYGTEST_MODERN_USED_
-#define dMYGTEST_MODERN_USED_ 001 PRE
+#define dMYGTEST_MODERN_USED_ 1
 
 #include <mygtest/test-list.hpp>
 #include <mygtest/private/extension.hpp>
