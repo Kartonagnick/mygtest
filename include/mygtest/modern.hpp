4b38d5c9093e8490967ba691e21ec39744115a52
Kartonagnick <Kartonagnick@gmail.com>
2023-07-31 15:25:38 +0300

    add: include/mygtest/modern.hpp (002) PRE

---
+++ include/mygtest/modern.hpp
@@ -0,0 +1,18 @@
+// --- local/mygtest                                                [modern.hpp]
+// [2023-08-05][19:00:00] 002 Kartonagnick PRE
+//   --- Katonagnick/mygtest                                        [modern.hpp]
+//   [2021-06-08][15:00:00] 002 Kartonagnick
+//   [2021-06-05][12:10:00] 001 Kartonagnick
+//==============================================================================
+//==============================================================================
+
+#pragma once
+#ifndef dMYGTEST_MODERN_USED_
+#define dMYGTEST_MODERN_USED_ 2
+
+#include "test-list.hpp"
+#include "private/extension.hpp"
+
+//==============================================================================
+//==============================================================================
+#endif // dMYGTEST_MODERN_USED_
