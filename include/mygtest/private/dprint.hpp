591f94344de92244af71009a9c00880d7ffc69c6
Kartonagnick <Kartonagnick@gmail.com>
2023-07-31 13:43:28 +0300

    upd: include/mygtest/private/dprint.hpp (002) PRE

--- include/mygtest/private/dprint.hpp
+++ include/mygtest/private/dprint.hpp
@@ -1,5 +1,5 @@
 // --- local/mygtest                                                [dprint.hpp]
-// [2023-07-31][19:00:00] 002 Kartonagnick PRE
+// [2023-08-05][19:00:00] 002 Kartonagnick PRE
 //   --- Katonagnick/mygtest                                        [dprint.hpp]
 //   [2021-06-08][15:00:00] 002 Kartonagnick
 //   [2021-06-05][12:10:00] 001 Kartonagnick
